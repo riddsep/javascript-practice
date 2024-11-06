f838c272d0106ed210c02bb87a32bf056555ca8d
riddsep <ridoseptiawan00@gmail.com>
2024-11-06 17:05:48 +0700

    Destructuring Array

--- 03-Data Structures-Modern-Operators-and-Strings/script.js
+++ 03-Data Structures-Modern-Operators-and-Strings/script.js
@@ -224,3 +224,21 @@ const books = [
     highlighted: true,
   },
 ];
+
+// Destructuring Array
+
+// const [firstBook, secondBook] = books;
+// const [, , thirdBook] = books;
+// console.log(firstBook, secondBook, thirdBook);
+
+// const ratings = [
+//   ["rating", 4.19],
+//   ["ratingsCount", 144584],
+// ];
+
+// const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
+
+// const ratingStars = [63405, 1808];
+// const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(threeStarRatings);
