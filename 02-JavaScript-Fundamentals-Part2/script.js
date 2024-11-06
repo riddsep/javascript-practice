949793a2c31a958189004f2a9b373f9c1ec1e2b2
riddsep <ridoseptiawan00@gmail.com>
2024-11-06 20:10:47 +0700

    Javascript fundamental part 2

---
+++ 02-JavaScript-Fundamentals-Part2/script.js
@@ -0,0 +1,16 @@
+// const describeCountry = function (country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// };
+
+// console.log(describeCountry("Indonesia", 250, "Jakarta"));
+
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
+
+// console.log(percentageOfWorld1(250));
+// console.log(percentageOfWorld2(550));
