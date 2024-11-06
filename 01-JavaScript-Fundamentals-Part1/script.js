4e08f1a218a4448b66021864862ab6837ea7b021
riddsep <ridoseptiawan00@gmail.com>
2024-11-06 18:41:00 +0700

    Javascript Fundamental Part 1

--- 01-JavaScript-Fundamentals-Part1/script.js
+++ 01-JavaScript-Fundamentals-Part1/script.js
@@ -7,3 +7,13 @@
 // let language;
 
 // console.log(isIsland, typeof language);
+
+// let population = "250000000";
+// population++;
+// const divide = population / 2;
+// console.log(divide);
+
+// const finlandiaPop = 6000000;
+// const IndonesiaPop = 2500000;
+
+// console.log(IndonesiaPop > finlandiaPop);
