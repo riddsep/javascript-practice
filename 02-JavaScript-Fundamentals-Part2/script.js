55d8b192b31d1c137abbeaf284c55e7badf5f602
riddsep <ridoseptiawan00@gmail.com>
2024-11-06 20:19:15 +0700

    Javascript fundamental part 2

--- 02-JavaScript-Fundamentals-Part2/script.js
+++ 02-JavaScript-Fundamentals-Part2/script.js
@@ -14,3 +14,20 @@
 
 // console.log(percentageOfWorld1(250));
 // console.log(percentageOfWorld2(550));
+
+// const percentageOfWorld2 = (population) => {
+//   return (population / 7900) * 100;
+// };
+// console.log(percentageOfWorld2(550));
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const describePopulation = (country, population) => {
+  const percentage = percentageOfWorld1(population);
+  return `${country} has ${population} million people, which is about ${percentage.toFixed(
+    2
+  )}% of the world`;
+};
+console.log(describePopulation("Indonesia", 1441));
