4644569c892446138a2c16bf1175487dc712490b
riddsep <ridoseptiawan00@gmail.com>
2024-11-06 19:02:07 +0700

    Javascript Fundamental Part 1

--- 01-JavaScript-Fundamentals-Part1/script.js
+++ 01-JavaScript-Fundamentals-Part1/script.js
@@ -17,3 +17,51 @@
 // const IndonesiaPop = 2500000;
 
 // console.log(IndonesiaPop > finlandiaPop);
+
+// const portugalPop = 6000000;
+// const indonesiaPop = 2500000;
+// if (indonesiaPop > portugalPop) {
+//   console.log(`Portugal's population is ${indonesiaPop} million below average`);
+// }
+
+// const myCountryNeighbour = Number(
+//   prompt("How many neighbour countries does your contry have?")
+// );
+
+// if (myCountryNeighbour == 1) {
+//   console.log("Only 1 border!");
+// } else if (myCountryNeighbour > 1) {
+//   console.log("More than 1 border");
+// } else {
+//   console.log("No borders");
+// }
+
+// const language = "arabic";
+
+// switch (language) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//   default:
+//     console.log("Great language too :D");
+//     break;
+// }
+const population = 34;
+const str =
+  population > 33
+    ? "Portugal's population is above average"
+    : "Portugal's population is below average";
+console.log(str);
