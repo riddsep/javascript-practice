a414a9369d4925ca219e9c5d903c6d7872fb1dba
riddsep <ridoseptiawan00@gmail.com>
2024-11-06 18:33:01 +0700

    Javascript Fundamental Part 1

---
+++ 01-JavaScript-Fundamentals-Part1/script.js
@@ -0,0 +1,9 @@
+// const country = "Indonesia";
+// const continent = "South asia";
+// const population = "250000000";
+// console.log(country, continent, population);
+
+// const isIsland = true;
+// let language;
+
+// console.log(isIsland, typeof language);
