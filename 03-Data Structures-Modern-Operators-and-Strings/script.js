a8d68d0e8af4c05b41c31fc0bdae5d46070f28e9
riddsep <ridoseptiawan00@gmail.com>
2024-11-08 23:50:15 +0700

    minor changes

--- 03-Data Structures-Modern-Operators-and-Strings/script.js
+++ 03-Data Structures-Modern-Operators-and-Strings/script.js
@@ -293,9 +293,8 @@ const books = [
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-const departure = flights.split("+");
-for (const item of departure) {
-  const departures = item
+for (const flight of flights.split("+")) {
+  const departures = flight
     .replaceAll("_", " ")
     .replaceAll(";", " ")
     .trimStart()
