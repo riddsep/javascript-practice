04510a90e624d4f66f0abe4c2d668faab3ae7033
riddsep <ridoseptiawan00@gmail.com>
2024-11-09 07:03:02 +0700

    minor changes

--- 03-Data Structures-Modern-Operators-and-Strings/script.js
+++ 03-Data Structures-Modern-Operators-and-Strings/script.js
@@ -290,14 +290,16 @@ const books = [
 // };
 // spellWord("Javascript");
 
-// const flights =
-//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-// for (const flight of flights.split("+")) {
-//   const onFlight = flight
-//     .replaceAll("_", " ")
-//     .replaceAll(";", " ")
-//     .trimStart()
-//     .split(" ");
-//   console.log(onFlight);
-// }
+const getCode = (str) => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  const output = `${type.startsWith("_Delayed") ? "✅" : ""}${type.replaceAll(
+    "_",
+    " "
+  )} ${getCode(from)} to ${getCode(to)} (${time})`.padStart(36);
+  console.log(output);
+}
