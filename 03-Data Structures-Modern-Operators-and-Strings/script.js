fd08fccda59b8cd65fe56117bfe28a3d50edc4ec
riddsep <ridoseptiawan00@gmail.com>
2024-11-10 23:50:34 +0700

    minor changes

--- 03-Data Structures-Modern-Operators-and-Strings/script.js
+++ 03-Data Structures-Modern-Operators-and-Strings/script.js
@@ -303,9 +303,3 @@ const books = [
 //   )} ${getCode(from)} to ${getCode(to)} (${time})`.padStart(36);
 //   console.log(output);
 // }
-// const poll = {
-//   question: "What is your favourite programming language?",
-//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-//   // This generates [0, 0, 0, 0]. More in the next section!
-//   answers: new Array(4).fill(0),
-// };
