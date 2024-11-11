72481b99c82e8e8f6f400f22da57356d8e432ea3
riddsep <ridoseptiawan00@gmail.com>
2024-11-11 21:52:09 +0700

    minor changes

--- 01-JavaScript-Fundamentals-Part1/script.js
+++ 01-JavaScript-Fundamentals-Part1/script.js
@@ -1,17 +1,17 @@
-const checkDogs = function (dogsJulia, dogsKate) {
-  const dogsJuliaCorrect = dogsJulia.slice(1, -2);
-  const bothDogs = [...dogsJuliaCorrect, ...dogsKate];
-  console.log(bothDogs);
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrect = dogsJulia.slice(1, -2);
+//   const bothDogs = [...dogsJuliaCorrect, ...dogsKate];
+//   console.log(bothDogs);
 
-  bothDogs.forEach(function (dog, i) {
-    const adultOrPuppy =
-      dog >= 5
-        ? `Dog number ${i + 1} is an adult, and is ${dog} years old`
-        : `Dog number ${i + 1} is still a puppy🐶`;
-    console.log(adultOrPuppy);
-  });
-};
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+//   bothDogs.forEach(function (dog, i) {
+//     const adultOrPuppy =
+//       dog >= 5
+//         ? `Dog number ${i + 1} is an adult, and is ${dog} years old`
+//         : `Dog number ${i + 1} is still a puppy🐶`;
+//     console.log(adultOrPuppy);
+//   });
+// };
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
-console.log("-----------");
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// console.log("-----------");
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
