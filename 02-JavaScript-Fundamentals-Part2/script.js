46ce0277efae6210dc2ee8878266fff4d8004963
riddsep <ridoseptiawan00@gmail.com>
2024-11-13 21:13:28 +0700

    minor changes

--- 02-JavaScript-Fundamentals-Part2/script.js
+++ 02-JavaScript-Fundamentals-Part2/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// const describeCountry = function (country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// };
+
+// console.log(describeCountry("Indonesia", 255, "Jakarta"));
+
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+// console.log(percentageOfWorld1(1441));
+
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
+// console.log(percentageOfWorld2(1441));
+
+// const percentageOfWorld3 = (population) => {
+//   return (population / 7900) * 100;
+// };
