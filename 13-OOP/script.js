1c440cf2d0ca88255218694ae96260177715afa9
riddsep <ridoseptiawan00@gmail.com>
2024-11-17 21:09:09 +0700

    minor changes

--- 13-OOP/script.js
+++ 13-OOP/script.js
@@ -1,14 +1,14 @@
-// const Hero = function (name, difficulty) {
-//   this.name = name;
-//   this.difficulty = difficulty;
-//   this.meta = false;
-// };
+const Hero = function (name, difficulty) {
+  this.name = name;
+  this.difficulty = difficulty;
+  this.meta = false;
+};
 
-// Hero.prototype.isMeta = function (statusMeta = false) {
-//   this.meta = statusMeta;
-//   return `You created ${this.name} with difficulty ${
-//     this.difficulty
-//   }, and it is currently ${this.meta ? "a Top pick now!" : "Not OP"}`;
-// };
+Hero.prototype.isMeta = function (statusMeta = false) {
+  this.meta = statusMeta;
+  return `You created ${this.name} with difficulty ${
+    this.difficulty
+  }, and it is currently ${this.meta ? "a Top pick now!" : "Not OP"}`;
+};
 
-// const hayabusa = new Hero("Hayabusa", "Hard");
+const hayabusa = new Hero("Hayabusa", "Hard");
