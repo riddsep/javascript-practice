346ff1a5c080e8f6f91bd1dda96ace0a63041137
riddsep <ridoseptiawan00@gmail.com>
2024-11-18 06:40:30 +0700

    minor changes

--- 13-OOP/script.js
+++ 13-OOP/script.js
@@ -1,19 +1,15 @@
-const Hero = function (name, difficulty) {
+const Person = function (name, birthYear) {
   this.name = name;
-  this.difficulty = difficulty;
-  this.meta = false;
+  this.birthYear = birthYear;
 };
 
-Hero.prototype.isMeta = function (statusMeta = false) {
-  this.meta = statusMeta;
-  return `You created ${this.name} with difficulty ${
-    this.difficulty
-  }, and it is currently ${this.meta ? "a Top pick now!" : "Not OP"}`;
+Person.prototype.calcAge = function () {
+  return 2030 - this.birthYear;
 };
 
-const hayabusa = new Hero("Hayabusa", "Easy");
-const fanny = new Hero("fanny", "God");
-
-console.log(hayabusa.__proto__ === fanny.__proto__);
-
-console.log(hayabusa.isMeta(true));
+const rido = new Person("Septiawan", 2001);
+console.log(
+  `Hi my name is ${rido.name}. im born in ${
+    rido.birthYear
+  }. in 2030 i was ${rido.calcAge()}`
+);
