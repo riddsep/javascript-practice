0bdaf61251058a67326f44e70e26d55657f5974c
riddsep <ridoseptiawan00@gmail.com>
2024-11-18 06:40:58 +0700

    minor changes

--- 13-OOP/script.js
+++ 13-OOP/script.js
@@ -1,15 +1,15 @@
-const Person = function (name, birthYear) {
-  this.name = name;
-  this.birthYear = birthYear;
-};
+// const Person = function (name, birthYear) {
+//   this.name = name;
+//   this.birthYear = birthYear;
+// };
 
-Person.prototype.calcAge = function () {
-  return 2030 - this.birthYear;
-};
+// Person.prototype.calcAge = function () {
+//   return 2030 - this.birthYear;
+// };
 
-const rido = new Person("Septiawan", 2001);
-console.log(
-  `Hi my name is ${rido.name}. im born in ${
-    rido.birthYear
-  }. in 2030 i was ${rido.calcAge()}`
-);
+// const rido = new Person("Septiawan", 2001);
+// console.log(
+//   `Hi my name is ${rido.name}. im born in ${
+//     rido.birthYear
+//   }. in 2030 i was ${rido.calcAge()}`
+// );
