5dbb6a765a93fc9dd1f807fcf4f50175fd813fc3
riddsep <ridoseptiawan00@gmail.com>
2024-11-18 06:46:15 +0700

    minor changes

--- 13-OOP/script.js
+++ 13-OOP/script.js
@@ -13,3 +13,23 @@
 //     rido.birthYear
 //   }. in 2030 i was ${rido.calcAge()}`
 // );
+
+const Car = function (brand, speed) {
+  this.brand = brand;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  return (this.speed += 10);
+};
+
+Car.prototype.break = function () {
+  return (this.speed -= 5);
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+
+console.log(bmw.accelerate());
+console.log(bmw.accelerate());
+console.log(bmw.break());
