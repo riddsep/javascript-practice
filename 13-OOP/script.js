89f510c8b120cb33b1c15a7cadd39da7d0f3b73e
riddsep <ridoseptiawan00@gmail.com>
2024-11-18 09:14:22 +0700

    minor changes

--- 13-OOP/script.js
+++ 13-OOP/script.js
@@ -34,28 +34,28 @@
 // console.log(bmw.accelerate());
 // console.log(bmw.break());
 
-class Person {
-  constructor(name, birthday, job) {
-    this.name = name;
-    this.birthday = birthday;
-    this.job = job;
-  }
+// class Person {
+//   constructor(name, birthday, job) {
+//     this.name = name;
+//     this.birthday = birthday;
+//     this.job = job;
+//   }
 
-  calcAge() {
-    return 2030 - this.birthday;
-  }
+//   calcAge() {
+//     return 2030 - this.birthday;
+//   }
 
-  currentJob() {
-    return `Hi!. my current job is ${this.job}`;
-  }
+//   currentJob() {
+//     return `Hi!. my current job is ${this.job}`;
+//   }
 
-  resume() {
-    return `Hi, my name is ${this.name}. Im ${this.calcAge()}`;
-  }
-}
+//   resume() {
+//     return `Hi, my name is ${this.name}. Im ${this.calcAge()}`;
+//   }
+// }
 
-const rido = new Person("Rido", 2001, "Software Engineer");
+// const rido = new Person("Rido", 2001, "Software Engineer");
 
-console.log(rido);
-console.log(rido.calcAge());
-console.log(rido.resume());
+// console.log(rido);
+// console.log(rido.calcAge());
+// console.log(rido.resume());
