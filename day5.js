fc542bf90d6642b34140a5d2e6af32a28ae17054
riddsep <ridoseptiawan00@gmail.com>
2024-11-19 18:16:04 +0700

    minor changes

--- day5.js
+++ day5.js
@@ -1,2 +1,24 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+"use strict";
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 // console.log(arr.length);
+
+// const first = arr[0];
+// const last = arr.at(-1);
+// const middleIndex = Math.floor(arr.length / 2);
+// const middle =
+//   arr.length % 2 === 0
+//     ? [arr[middleIndex - 1], arr[middleIndex]]
+//     : arr[middleIndex];
+// console.log(first, ...middle, last);
+
+// let text =
+//   "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+// const word = text.replace(/[,.]/g, "").split(" ");
+
+// console.log(word.length);
+// const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+// shoppingCart.unshift("Meat");
+// shoppingCart.push("Sugar");
+// shoppingCart.splice(-2, 1);
+// shoppingCart[shoppingCart.indexOf("Tea")] = "Green Tea";
+// console.log(shoppingCart);
