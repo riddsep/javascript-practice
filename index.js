c9f1a349a24cf3c8b98dc8499104b27cabc8fc3f
riddsep <ridoseptiawan00@gmail.com>
2024-11-20 09:58:11 +0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+Operator Logika
+
+Operator logika terdiri dari &&, ||, !, ??, meskipun disebut operator logika, operator ini tidak selalu menghasilkan nilai boolean ,tetapi hasilnya juga dapat berupa jenis nilai yang lain.
+
+OR ||
+Di pemrograman classic operator or || digunakan untuk boolean saja, tetapi di javscript ini  
+
+*/
