8e07469133de92b8cec3d22820532aeb1727cd07
riddsep <ridoseptiawan00@gmail.com>
2024-11-20 21:07:36 +0700

    init

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ OR ||
 Di pemrograman classic operator or || digunakan untuk boolean saja, tetapi di javscript ini  
 
 */
+
+function showMessage(from, text) {
+  // parameters: from, text
+  alert(from + ": " + text);
+}
+
+showMessage("Ann", "Hello!"); // Ann: Hello! (*)
+showMessage("Ann", "What's up?"); // Ann: What's up? (**)
