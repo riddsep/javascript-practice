f393c0aa06ce6bdc446fb7fc1d19c7d32259c347
riddsep <ridoseptiawan00@gmail.com>
2024-11-21 09:13:56 +0700

    minor changes

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-let a = 2;
 
-switch (a) {
-  case 0:
-    console.log(a);
-    break;
-  case 1:
-    console.log(a);
-    break;
-  case 2:
-  case 3:
-    a = 4;
-    console.log(a);
-    break;
-}
