32d7540805e048720c29bf4470234a1cd8acda2a
riddsep <ridoseptiawan00@gmail.com>
2024-11-25 20:24:20 +0700

    minor changes

--- index.js
+++ index.js
@@ -409,20 +409,77 @@ const books = [
 //   }
 // }
 
-const bookAuthors = [...books[0].author];
+// const bookAuthors = [...books[0].author];
 
-const spellWord = function (str) {
-  console.log(...str);
+// const spellWord = function (str) {
+//   console.log(...str);
+// };
+// spellWord("JavaScript");
+
+// const {
+//   keywords: [mainKeyword, ...rest],
+// } = books[0];
+
+// const { publisher: bookPublisher, ...restofbo } = books[1];
+
+// const printBookAuthorsCount = function (title, ...authors) {
+//   console.log(`The book "${title}" has ${authors.length} authors`);
+// };
+// printBookAuthorsCount("Algorithms", "Robert Sedgewick", "Kevin Wayne");
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
-spellWord("JavaScript");
 
-const {
-  keywords: [mainKeyword, ...rest],
-} = books[0];
+console.log(game.scored);
 
-const { publisher: bookPublisher, ...restofbo } = books[1];
+for (const [i, el] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${el}`);
+}
 
-const printBookAuthorsCount = function (title, ...authors) {
-  console.log(`The book "${title}" has ${authors.length} authors`);
-};
-printBookAuthorsCount("Algorithms", "Robert Sedgewick", "Kevin Wayne");
+let avg = 0;
+for (const odd of Object.values(game.odds)) {
+  avg += odd;
+}
+console.log(avg);
+
+for (const { team, odd } of Object.entries(game.odds)) {
+  console.log(team);
+  console.log(`Odd of `);
+}
