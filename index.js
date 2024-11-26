b3d77f2a21bedec65374e0321c6f1bd68ab61a30
riddsep <ridoseptiawan00@gmail.com>
2024-11-26 18:21:30 +0700

    minor changes

--- index.js
+++ index.js
@@ -539,26 +539,26 @@ const books = [
 
 // console.log(question.get(answer === "Javascript"));
 
-const gameEvents = new Map([
-  [17, "⚽ GOAL"],
-  [36, "🔁 Substitution"],
-  [47, "⚽ GOAL"],
-  [61, "🔁 Substitution"],
-  [64, "🔶 Yellow card"],
-  [69, "🔴 Red card"],
-  [70, "🔁 Substitution"],
-  [72, "🔁 Substitution"],
-  [76, "⚽ GOAL"],
-  [80, "⚽ GOAL"],
-  [92, "🔶 Yellow card"],
-]);
+// const gameEvents = new Map([
+//   [17, "⚽ GOAL"],
+//   [36, "🔁 Substitution"],
+//   [47, "⚽ GOAL"],
+//   [61, "🔁 Substitution"],
+//   [64, "🔶 Yellow card"],
+//   [69, "🔴 Red card"],
+//   [70, "🔁 Substitution"],
+//   [72, "🔁 Substitution"],
+//   [76, "⚽ GOAL"],
+//   [80, "⚽ GOAL"],
+//   [92, "🔶 Yellow card"],
+// ]);
 
-const events = [...gameEvents.values()];
-console.log(new Set(events));
+// const events = [...gameEvents.values()];
+// console.log(new Set(events));
 
-console.log(`An event happened, on 
-average, every ${90 / gameEvents.size} minutes`);
+// console.log(`An event happened, on
+// average, every ${90 / gameEvents.size} minutes`);
 
-for (const [i, el] of gameEvents.entries()) {
-  console.log(`[${i <= 45 ? "FIRST HALF" : "SECOND HALF"}] ${i}: ${el}`);
-}
+// for (const [i, el] of gameEvents.entries()) {
+//   console.log(`[${i <= 45 ? "FIRST HALF" : "SECOND HALF"}] ${i}: ${el}`);
+// }
