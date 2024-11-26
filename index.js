b718a3ca1fb81a90de696a96e8e23d3d0ba916a0
riddsep <ridoseptiawan00@gmail.com>
2024-11-26 08:31:08 +0700

    minor changes

--- index.js
+++ index.js
@@ -538,3 +538,17 @@ const books = [
 // const answer = question.get(+question.get("question"));
 
 // console.log(question.get(answer === "Javascript"));
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
