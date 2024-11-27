088d64a72e8513259858332b7bf080978944cbc0
riddsep <ridoseptiawan00@gmail.com>
2024-11-27 07:43:03 +0700

    minor changes

--- index.js
+++ index.js
@@ -583,16 +583,16 @@ const books = [
 //   }
 // });
 
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-for (const f of flights.split("+")) {
-  const [i, j, k, l] = f.split(";");
+// for (const f of flights.split("+")) {
+//   const [i, j, k, l] = f.split(";");
 
-  const output = `${i.replaceAll("_", " ")} from ${j
-    .slice(0, 3)
-    .toUpperCase()} to ${k.slice(0, 3).toUpperCase()} (${l.replace(":", "h")})`;
-  console.log(
-    `${output.startsWith("Delayed") ? "🔥" : ""}${output.padStart(45)}`
-  );
-}
+//   const output = `${i.replaceAll("_", " ")} from ${j
+//     .slice(0, 3)
+//     .toUpperCase()} to ${k.slice(0, 3).toUpperCase()} (${l.replace(":", "h")})`;
+//   console.log(
+//     `${output.startsWith("Delayed") ? "🔥" : ""}${output.padStart(45)}`
+//   );
+// }
