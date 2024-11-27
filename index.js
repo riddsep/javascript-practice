edafc2af316beaee262feeb13d519ded0691efa1
riddsep <ridoseptiawan00@gmail.com>
2024-11-28 06:14:17 +0700

    minor changes

--- index.js
+++ index.js
@@ -645,9 +645,9 @@ const books = [
 // const getNameL = getName.bind(lancelot);
 
 // getNameL("Fighter");
-const poll = {
-  question: "What is your favourite programming language?",
-  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-  // This generates [0, 0, 0, 0]. More in the next section!
-  answers: new Array(4).fill(0),
-};
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
+// };
