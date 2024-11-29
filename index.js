66fe6a5081005134289782d189ba1204572fa6f8
riddsep <ridoseptiawan00@gmail.com>
2024-11-29 18:20:38 +0700

    minor changes

--- index.js
+++ index.js
@@ -802,3 +802,20 @@ const books = [
 // }
 
 // console.log(generateFibonacci(5)); // Output: [0, 1, 1, 2, 3]
+
+// function highAndLow(numbers) {
+//   // ...
+//   const nums = numbers.split(" ").map(Number);
+//   let min = nums[0];
+//   let max = nums[0];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] < min) {
+//       min = nums[i];
+//     } else if (nums[i] > max) {
+//       max = nums[i];
+//     }
+//   }
+//   return `${max} ${min}`;
+// }
+// console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
