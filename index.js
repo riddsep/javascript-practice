7afce95012a842e835aa48d485b69d37879dd8bc
riddsep <ridoseptiawan00@gmail.com>
2024-12-01 11:07:18 +0700

    minor changes

--- index.js
+++ index.js
@@ -919,3 +919,24 @@ const books = [
 // rido.calcAge();
 // console.log(rido);
 // Person.hi();
+
+class Iphone {
+  constructor(type, price, year) {
+    this.type = type;
+    this.price = price;
+    this.year = year;
+  }
+
+  set type(t) {
+    if (t.includes(11)) this._type = t;
+  }
+  get type() {
+    return this._type;
+  }
+}
+
+const ip11 = new Iphone("Iphone 12", 1000, 2019);
+ip11.type = "Ip 11";
+
+ip11.type;
+console.log(ip11);
