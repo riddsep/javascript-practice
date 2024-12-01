34eed2cc882d74f31eda6936c550a72555f8bc93
riddsep <ridoseptiawan00@gmail.com>
2024-12-01 19:54:26 +0700

    minor changes

--- index.js
+++ index.js
@@ -973,3 +973,41 @@ const books = [
 // ford.brake();
 // ford.speedUS = 50;
 // console.log(ford);
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  console.log((this.speed += 10));
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
+Car.prototype.brake = function () {
+  console.log((this.speed -= 5));
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.charge--;
+  this.speed += 20;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+
+tesla.chargeBattery(90);
+tesla.accelerate();
+tesla.brake();
+console.log(tesla);
