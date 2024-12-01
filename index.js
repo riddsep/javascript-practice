4df0105564e39c1ac961d1953aa4606d893fa859
riddsep <ridoseptiawan00@gmail.com>
2024-12-01 10:07:20 +0700

    minor changes

--- index.js
+++ index.js
@@ -887,18 +887,18 @@ const books = [
 // bmw.brake();
 // bmw.brake();
 
-class Car {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelerate() {
-    this.speed += 10;
-    console.log(`${this.make} going on ${this.speed} km/h`);
-  }
-}
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} going on ${this.speed} km/h`);
+//   }
+// }
 
-const bmw = new Car("BMW", 120);
-bmw.accelerate();
-bmw.accelerate();
+// const bmw = new Car("BMW", 120);
+// bmw.accelerate();
+// bmw.accelerate();
