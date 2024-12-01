c1aa37b88752569d6b37603184e4b42345291533
riddsep <ridoseptiawan00@gmail.com>
2024-12-01 15:48:12 +0700

    minor changes

--- index.js
+++ index.js
@@ -941,35 +941,35 @@ const books = [
 // ip11.type;
 // console.log(ip11);
 
-class Car {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelerate() {
-    this.speed += 10;
-    console.log(`${this.make} going on ${this.speed} km/h`);
-  }
-  brake() {
-    this.speed -= 5;
-    console.log(`${this.make} going on ${this.speed} km/h`);
-  }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} going on ${this.speed} km/h`);
+//   }
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`${this.make} going on ${this.speed} km/h`);
+//   }
 
-  get speedUS() {
-    return (this.speed /= 1.6);
-  }
+//   get speedUS() {
+//     return (this.speed /= 1.6);
+//   }
 
-  set speedUS(speed) {
-    this.speed = speed * 1.6;
-  }
-}
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
 
-const ford = new Car("Ford", 120);
+// const ford = new Car("Ford", 120);
 
-console.log(ford.speedUS);
-ford.accelerate();
-ford.accelerate();
-ford.brake();
-ford.speedUS = 50;
-console.log(ford);
+// console.log(ford.speedUS);
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.speedUS = 50;
+// console.log(ford);
